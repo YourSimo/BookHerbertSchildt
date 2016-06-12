a7afb212ff63b264ea4daf6cae24d715154bdc6b
Simone Cavicchioli <s.cavicchioli@studenti.unibs.it>
2016-06-12 11:46:17 +0200

    Add Herbert Schildt Code

---
+++ src/com/schildt/java/ch08/es1_simpleInheritance/A.java
@@ -0,0 +1,13 @@
+package com.schildt.java.ch08.es1_simpleInheritance;
+
+//A simple example of inheritance.
+
+//Create a superclass.
+class A {
+	int i, j;
+
+	void showij() {
+		System.out.println("i and j: " + i + " " + j);
+	}
+}
+
